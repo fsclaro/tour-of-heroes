594f5033c5cdae1e727a76132370ec9160eef5f6
Fernando S Claro <fernando.claro@embras.net>
2021-12-15 23:21:44 -0300

    criado componente heros

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.scss'],
+})
+export class HerosComponent implements OnInit {
+  hero = 'Wolverine';
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+}
